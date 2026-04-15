5b8dc4ae1b818bcd0346659bdfd192e6964449dd
Kasjan Karpiński <kasjan.karpinski@gmail.com>
2026-04-15 12:12:35 +0200

    feat: update dino-data structure and introduce dynamic target calculation in config

--- src/dinopark/config.py
+++ src/dinopark/config.py
@@ -6,5 +6,8 @@ DATA_FILE = Path(__file__).parent / "dino-data.json"
 # Conversion rates: how many lvl 1 dinos are needed for higher level
 BALANCES = {6: 32, 5: 16, 4: 8, 3: 4, 2: 2, 1: 1}
 
-# The number shows how many dinos are needed to create a full set
-TARGET = BALANCES[6] * 2 - 1
+# The number of dinos needed to get the first totem
+TARGET_FIRST_TOTEM = sum(BALANCES.values())
+
+# Number of dinos needed to get the remaining totems / golden chest
+TARGET_OTHER_TOTEMS = BALANCES[max(BALANCES.keys())]
--- src/dinopark/logic.py
+++ src/dinopark/logic.py
@@ -4,14 +4,6 @@ def calculate_possessed_sum(
     """
     Calculates the total number of units you have by converting dinosaur
     levels to their base values ​from the balances.
-
-    Parameters:
-    counts (dict[int, int]): Dictionary where key is level
-    and value is quantity.
-    balances (dict[int, int]):: Dictionary where key is level
-    and value is its unit worth.
-    Returns:
-    int: Total number of level 1 units
     """
     total = 0
     for lvl, quantity in counts.items():
@@ -24,14 +16,6 @@ def get_missing_amount(possessed_sum: int, target: int) -> int:
     Calculates the missing number of dinosaurs to obtain the totem.
 
     Negative possessed_sum values are treated as 0.
-
-    Parameters:
-    possessed_sum: (int): Current number of units the user has.
-    target: (int): Required number of units to obtain the totem.
-
-    Returns:
-    int: How many level 1 units are needed to get the totem
-    (never less than 0).
     """
     if possessed_sum < 0:
         possessed_sum = 0
